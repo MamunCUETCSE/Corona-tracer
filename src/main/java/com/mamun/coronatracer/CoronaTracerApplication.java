30733f23c6429c91437f33270cc90bdc5ca606e4
MamunCUETCSE <112986954+MamunCUETCSE@users.noreply.github.com>
2022-09-28 12:33:43 +0600

    Add files via upload

---
+++ src/main/java/com/mamun/coronatracer/CoronaTracerApplication.java
@@ -0,0 +1,13 @@
+package com.mamun.coronatracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamun/coronatracer/CoronaTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamun.coronatracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
